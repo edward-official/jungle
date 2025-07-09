4dc650bf8e1a17221e69922f8f60f4c48ae83d3a
edwardlee <openmoresome@gmail.com>
2025-07-10 00:30:55 +0900

    project 1 version 1

--- admission/struggle/project1/app.py
+++ admission/struggle/project1/app.py
@@ -2,18 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from bson import ObjectId
+from flask.json.provider import JSONProvider
 
 app = Flask(__name__)
 mongo = MongoClient("localhost", 27017)
 database = mongo.jungle
 collection = database.movies
-
-arrangeMethod = "releasedYearDescending"
-"""
-default: database order (desecnding)
-likes: descending
-releasedYear: ascending
-"""
 
 @app.route('/', methods=["GET"])
 def home():
@@ -21,10 +16,45 @@ def home():
 
 @app.route("/listup", methods=["GET"])
 def listup():
-  movies = list(collection.find())
+  isLikeOrderPrior = request.args.get("isLikeOrderPrior")
+  yearOrder = int(request.args.get("yearOrder"))
+  likeOrder = int(request.args.get("likeOrder"))
+  movies = []
+  if isLikeOrderPrior=="true":
+    movies = list(collection.find().sort([("likes", likeOrder), ("releasedYear", yearOrder)]))
+  else:
+    movies = list(collection.find().sort([("releasedYear", yearOrder), ("likes", likeOrder)]))
   for movie in movies:
     movie["_id"] = str(movie["_id"])
   return jsonify({"result": "success", "movies": movies})
+
+@app.route("/like", methods=["POST"])
+def like():
+  movieID = request.form["movieID"]
+  selectedMovie = collection.find_one({"_id": ObjectId(movieID)}, {})
+  updatedLike = selectedMovie["likes"] + 1
+  print(updatedLike)
+  collection.update_one({"_id": ObjectId(movieID)}, {"$set": {"likes": updatedLike}})
+  return jsonify({"result": "success"})
+
+@app.route("/hide", methods=["POST"])
+def hide():
+  movieID = request.form["movieID"]
+  # print("movieID: " + movieID)
+  collection.update_one({"_id": ObjectId(movieID)}, {"$set": {"hidden": True}})
+  return jsonify({"result": "success"})
+
+@app.route("/unhide", methods=["POST"])
+def unhide():
+  movieID = request.form["movieID"]
+  collection.update_one({"_id": ObjectId(movieID)}, {"$set": {"hidden": False}})
+  return jsonify({"result": "success"})
+
+@app.route("/delete", methods=["POST"])
+def delete():
+  movieID = request.form["movieID"]
+  collection.delete_one({"_id": ObjectId(movieID)})
+  return jsonify({"result": "success"})
 
 
 if __name__ == '__main__':
--- admission/struggle/project1/static/script.js
+++ admission/struggle/project1/static/script.js
@@ -1,43 +1,118 @@
 $(document).ready(function (response) {
+  displayMovies();
+});
+
+let isHiddenConsidered = false;
+let isLikeOrderPrior = true;
+let yearOrder = -1;
+let likeOrder = -1;
+
+function displayMovies() {
+  $("#movie-container").empty();
   $.ajax({
     type: "GET",
     url: "/listup",
-    data: {},
+    data: {
+      isLikeOrderPrior: isLikeOrderPrior,
+      yearOrder: yearOrder,
+      likeOrder: likeOrder,
+    },
     success: function (response) {
+      $("#movie-container").cl;
       let movies = response["movies"];
-      displayMovies(movies);
+      for (let index = 0; index < movies.length; index++) {
+        let movie = movies[index];
+        if (movie["hidden"] == !isHiddenConsidered) continue;
+        let movieID = movie["_id"];
+        console.log(typeof movieID);
+        let title = movie["title"];
+        let releasedYear = movie["releasedYear"];
+        let runningTime = movie["runningTime"];
+        let likes = movie["likes"];
+        let buttons;
+        if (isHiddenConsidered) {
+          buttons = `<button class="button-1" onclick="buttonRestore('${movieID}')">♻️ restore</button>
+                    <button class="button-1" onclick="buttonDelete('${movieID}')">❌ delete</button>`;
+        } else {
+          buttons = `<button class="button-1" onclick="buttonLike('${movieID}')">👍 ${likes}</button>
+                    <button class="button-1" onclick="buttonHide('${movieID}')">❌ hide</button>`;
+        }
+        let appendee = `<div class="movie-element">
+                          <div>
+                            <span class="movie-title">${title}</span>
+                            <span class="movie-likes">${buttons}</span>
+                          </div>
+                          <div class="movie-releasedYear">${releasedYear}</div>
+                          <div class="movie-runningTime">${runningTime}</div>
+                        </div>`;
+        $("#movie-container").append(appendee);
+      }
     },
   });
-});
-
-function displayMovies(movies) {
-  for (let index = 0; index < movies.length; index++) {
-    let movie = movies[index];
-    if (movie["hidden"]) continue;
-    let movieID = movie["_id"];
-    let title = movie["title"];
-    let releasedYear = movie["releasedYear"];
-    let runningTime = movie["runningTime"];
-    let likes = movie["likes"];
-    let appendee = `<div class="movie-element">
-                      <div>
-                        <span class="movie-title">${title}</span>
-                        <span class="movie-likes">
-                          <button class="button-likes" onclick="buttonLike('${movieID}')">👍 ${likes}</button>
-                          <button class="button-hide" onclick="buttonHide('${movieID}')">❌ hide</button>
-                        </span>
-                      </div>
-                      <div class="movie-releasedYear">${releasedYear}</div>
-                      <div class="movie-runningTime">${runningTime}</div>
-                    </div>`;
-    $("#movie-container").append(appendee);
-  }
 }
 
 function buttonLike(movieID) {
   console.log("like: " + movieID);
+  $.ajax({
+    type: "POST",
+    url: "/like",
+    data: { movieID: movieID },
+    success: function (response) {
+      console.log("success");
+      displayMovies();
+    },
+  });
 }
 
 function buttonHide(movieID) {
   console.log("hide: " + movieID);
+  $.ajax({
+    type: "POST",
+    url: "/hide",
+    data: { movieID: movieID },
+    success: function (response) {
+      displayMovies();
+    },
+  });
+}
+
+function buttonRestore(movieID) {
+  $.ajax({
+    type: "POST",
+    url: "/unhide",
+    data: { movieID: movieID },
+    success: function (response) {
+      displayMovies();
+    },
+  });
+}
+
+function buttonDelete(movieID) {
+  $.ajax({
+    type: "POST",
+    url: "/delete",
+    data: { movieID: movieID },
+    success: function (response) {
+      displayMovies();
+    },
+  });
+}
+
+function convertMode() {
+  isHiddenConsidered = !isHiddenConsidered;
+  if (isHiddenConsidered) $("#button-mode").text("convert to visible movies");
+  else $("#button-mode").text("convert to hidden movies");
+  displayMovies();
+}
+
+function flipYearOrder() {
+  yearOrder = yearOrder * -1;
+  isLikeOrderPrior = false;
+  displayMovies();
+}
+
+function flipLikeOrder() {
+  likeOrder = likeOrder * -1;
+  isLikeOrderPrior = true;
+  displayMovies();
 }
