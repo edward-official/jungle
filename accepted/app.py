0665ba0579e102c64458dc34ff43f14428ac0919
edwardlee <openmoresome@gmail.com>
2025-08-25 19:43:05 +0900

    0825

--- accepted/app.py
+++ accepted/app.py
@@ -1,54 +1,31 @@
 from flask import Flask, render_template, jsonify, request
-import requests
-from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
-db = client.dbjungle
+app = Flask(__name__)
+mongoDB = MongoClient('localhost', 27017)
+database = mongoDB.openPL
 
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+  return render_template('language.html')
 
+@app.route("/login", methods=["GET"])
+def login():
+  id = request.args.get("id")
+  print(id)
+  return jsonify({"result": "success"})
 
-@app.route('/memo', methods=['POST'])
-def post_article():
-  url_receive = request.form['url_give']
-  comment_receive = request.form['comment_give']
+@app.route("/singup", methods=["POST"])
+def signup():
+  id = request.form["id"]
+  pw = request.form["pw"]
 
-  headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-  data = requests.get(url_receive, headers=headers)
-  soup = BeautifulSoup(data.text, 'html.parser')
+  user = {"id": id, "pw": pw}
+  database.users.insert_one(user)
 
-  og_image = soup.select_one('meta[property="og:image"]')
-  og_title = soup.select_one('meta[property="og:title"]')
-  og_description = soup.select_one('meta[property="og:description"]')
-
-  url_title = og_title['content']
-  url_description = og_description['content']
-  url_image = og_image['content']
-
-  article = {
-    'url': url_receive,
-    'title': url_title,
-    'desc': url_description,
-    'image': url_image,
-    'comment': comment_receive
-  }
-
-  db.articles.insert_one(article)
-
-  return jsonify({'result': 'success'})
-
-
-@app.route('/memo', methods=['GET'])
-def read_articles():
-  result = list(db.articles.find({}, {'_id': 0}))
-  return jsonify({'result': 'success', 'articles': result})
+  return jsonify({"result": "success"})
 
 
 if __name__ == '__main__':
--- accepted/static/script.js
+++ accepted/static/script.js
@@ -3,22 +3,34 @@ function opening() {
 }
 $(document).ready(opening());
 
-function logIn() {
-  let logIn = $("#log-in");
-  let idEmail = logIn.find("#id-email").val();
+function login() {
+  let loginForm = $("#log-in");
+  let id = loginForm.find("#id");
+  let pw = loginForm.find("#pw");
 
-  alert("log in: " + idEmail);
+  $.ajax({
+    type: "GET",
+    url: "/login",
+    data: { id: id.val() },
+    success: function (response) {
+      alert("api");
+    },
+  });
 
-  tagIdEmail.val("");
-  tagPw.val("");
+  id.val("");
+  pw.val("");
 }
 
-$.ajax({
-  type: "GET", // GET 방식으로 요청한다.
-  url: "http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99",
-  data: {}, // 요청하면서 함께 줄 데이터 (GET 요청시엔 비워두세요)
-  success: function (response) {
-    // 서버에서 준 결과를 response라는 변수에 담음
-    console.log(response); // 서버에서 준 결과를 이용해서 나머지 코드를 작성
-  },
-});
+function unveilSignup() {
+  let loginForm = $("#log-in");
+  let signupForm = $("#sign-up");
+  loginForm.addClass("hidden");
+  signupForm.removeClass("hidden");
+}
+
+function cancelSignup() {
+  let loginForm = $("#log-in");
+  let signupForm = $("#sign-up");
+  loginForm.removeClass("hidden");
+  signupForm.addClass("hidden");
+}
